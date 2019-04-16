c5e7a69488ddcad2e5a36c784f6a190415bcb322
Logan Yott <loganyott@gmail.com>
2019-04-15 22:45:49 -0600

    #45 Implement Favoriting

--- establish/src/components/Header/Favorite.js
+++ establish/src/components/Header/Favorite.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Storage } from "../../Storage";
 
-const Favorite = ({isFavorite = false, onFavorite}) =>
-  <div className='h-100 flex ml-auto' onClick={onFavorite}>
-    <img className='ml3 h-100 w-100' src={`/assets/actions/favorite${isFavorite ? '-active' : ''}.svg`} alt='favorite' />
-  </div>
+
+
+function Favorite({ id }) {
+  const [isFavorite, setFavorite] = useState(false);
+
+  useEffect(() => {
+    Storage.open()
+      .then(() => Storage.table('favorites').get(id)
+        .then(isFavorite => setFavorite(isFavorite && true)))
+  }, [id])
+
+  const onFavorite = async (id) => {
+    await Storage.open();
+    const favorites = Storage.table('favorites');
+    const isFavorite = await favorites.get(id);
+    if (isFavorite) {
+      setFavorite(false);
+      return favorites.delete(id);
+    } else {
+      setFavorite(true);
+      return favorites.add({id});
+    }
+  }
+
+  return (
+    <div className='h-100 flex ml-auto' onClick={() => onFavorite(id)}>
+      <img className='ml3 h-100 w-100' src={`/assets/actions/favorite${isFavorite ? '-active' : ''}.svg`} alt='favorite'/>
+    </div>
+  )
+}
 
 export default Favorite
